22d24bb8a29a2b419fc6788c848028176bea3b20
alexander <earthhuman41@gmail.com>
2020-01-27 21:35:00 +0200

    added styled  components

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 module.exports = {
   webpack: (config) => {
+    // aliases
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@components": path.join(__dirname, "src/components")
+      "@components": path.join(__dirname, "src/components"),
+      "@": path.join(__dirname, "src/")
     }
     return config;
   },
-}
+};
